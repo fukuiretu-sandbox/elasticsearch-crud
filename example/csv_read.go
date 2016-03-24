aac67c1574ef6f044a7ccffe3a25627f07fa8707
fukuiretu <fukuiretu@gmail.com>
2016-03-25 00:18:47 +0900

    modify

---
+++ example/csv_read.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+type hoge struct {
+	one   string
+	two   string
+	three string
+}
+
+func newHoge(record []string) *hoge {
+	result := &hoge{}
+	result.one = record[0]
+	result.two = record[1]
+	result.three = record[2]
+
+	return result
+}
+
+func main() {
+	fp, err := os.Open("fixtures/test.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	reader := csv.NewReader(fp)
+	reader.Comma = ','
+	reader.LazyQuotes = true
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		// hoge := &hoge{record[0], record[1], record[2]}
+		hoge := newHoge(record)
+		fmt.Println(hoge)
+	}
+}
