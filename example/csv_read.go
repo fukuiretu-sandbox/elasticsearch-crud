1d15173d8bc53c166f1e4709890d489f2d56eb15
fukuiretu <fukuiretu@gmail.com>
2016-03-26 13:37:31 +0900

    modify

--- example/csv_read.go
+++ example/csv_read.go
@@ -7,23 +7,92 @@ import (
 	"os"
 )
 
-type hoge struct {
-	one   string
-	two   string
-	three string
+type restaurant struct {
+	id               string
+	name             string
+	property         string
+	alphabet         string
+	nameKana         string
+	prefID           string
+	areaID           string
+	stationID1       string
+	stationTime1     string
+	stationDistance1 string
+	stationID2       string
+	stationTime2     string
+	stationDistance2 string
+	stationID3       string
+	stationTime3     string
+	stationDistance3 string
+	categoryID1      string
+	categoryID2      string
+	categoryID3      string
+	categoryID4      string
+	categoryID5      string
+	zip              string
+	address          string
+	northLatitude    string
+	eastLongitude    string
+	description      string
+	purpose          string
+	openMorning      string
+	openLunch        string
+	openLate         string
+	photoCount       string
+	specialCount     string
+	menuCount        string
+	fanCount         string
+	accessCount      string
+	createdOn        string
+	modifiedOn       string
+	closed           string
 }
 
-func newHoge(record []string) *hoge {
-	result := new(hoge)
-	result.one = record[0]
-	result.two = record[1]
-	result.three = record[2]
-
+func newRestaurant(record []string) *restaurant {
+	result := new(restaurant)
+	result.id = record[0]
+	result.name = record[1]
+	result.property = record[2]
+	result.alphabet = record[3]
+	result.nameKana = record[4]
+	result.prefID = record[5]
+	result.areaID = record[6]
+	result.stationID1 = record[7]
+	result.stationTime1 = record[8]
+	result.stationDistance1 = record[9]
+	result.stationID2 = record[10]
+	result.stationTime2 = record[11]
+	result.stationDistance2 = record[12]
+	result.stationID3 = record[13]
+	result.stationTime3 = record[14]
+	result.stationDistance3 = record[15]
+	result.categoryID1 = record[16]
+	result.categoryID2 = record[17]
+	result.categoryID3 = record[18]
+	result.categoryID4 = record[19]
+	result.categoryID5 = record[20]
+	result.zip = record[21]
+	result.address = record[22]
+	result.northLatitude = record[23]
+	result.eastLongitude = record[24]
+	result.description = record[25]
+	result.purpose = record[26]
+	result.openMorning = record[27]
+	result.openLunch = record[28]
+	result.openLate = record[29]
+	result.photoCount = record[30]
+	result.specialCount = record[31]
+	result.menuCount = record[32]
+	result.fanCount = record[33]
+	result.accessCount = record[34]
+	result.createdOn = record[35]
+	result.modifiedOn = record[36]
+	result.closed = record[37]
 	return result
 }
 
 func main() {
-	fp, err := os.Open("fixtures/test.csv")
+	fp, err := os.Open("fixtures/restaurants.csv")
 	if err != nil {
 		panic(err)
 	}
@@ -39,8 +108,8 @@ func main() {
 		} else if err != nil {
 			panic(err)
 		}
-		// hoge := &hoge{record[0], record[1], record[2]}
-		hoge := newHoge(record)
-		fmt.Println(hoge)
+
+		restaurant := newRestaurant(record)
+		fmt.Println(restaurant)
 	}
 }
--- index.go
+++ index.go
@@ -6,9 +6,9 @@ import (
 	"gopkg.in/olivere/elastic.v3"
 )
 
-type Tweet struct {
-	User    string
-	Message string
+type tweet struct {
+	user    string
+	message string
 }
 
 func main() {
@@ -27,16 +27,43 @@ func main() {
 	// }
 
 	// Add a document to the index
-	tweet := `{"user" : "ror", "message" : "fuck you"}`
-	_, err = client.Index().
-		Index("twitter").
-		Type("tweet").
-		BodyJson(tweet).
-		Do()
+	// tweet := `{"user" : "ror", "message" : "fuck you"}`
+	// single
+	// _, err = client.Index().
+	// 	Index("twitter").
+	// 	Type("tweet").
+	// 	BodyJson(tweet).
+	// 	Do()
+	// if err != nil {
+	// 	// Handle error
+	// 	panic(err)
+	// }
+
+	// bulk
+	tweet1 := tweet{user: "olivere", message: "Welcome to Golang and Elasticsearch."}
+	tweet2 := tweet{user: "sandrae", message: "Dancing all night long. Yeah."}
+	index1Req := elastic.NewBulkIndexRequest().Index("twitter").Type("tweet").Doc(tweet1)
+	index2Req := elastic.NewBulkIndexRequest().Index("twitter").Type("tweet").Doc(tweet2)
+
+	bulkRequest := client.Bulk()
+	bulkRequest = bulkRequest.Add(index1Req)
+	bulkRequest = bulkRequest.Add(index2Req)
+
+	// if bulkRequest.NumberOfActions() != 2 {
+	// 	panic("expected bulkRequest.NumberOfActions %d; got %d", 3, bulkRequest.NumberOfActions())
+	// }
+
+	bulkResponse, err := bulkRequest.Do()
 	if err != nil {
-		// Handle error
 		panic(err)
 	}
+	if bulkResponse == nil {
+		panic("expected bulkResponse to be != nil; got nil")
+	}
+
+	// if bulkRequest.NumberOfActions() != 0 {
+	// 	panic("expected bulkRequest.NumberOfActions %d; got %d", 0, bulkRequest.NumberOfActions())
+	// }
 
 	fmt.Println("end")
 }
